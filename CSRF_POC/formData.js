ec203776fb8329917701b134fc6d1dd8af61388f
JUNG SOON HO <ajsoon98@naver.com>
2026-02-26 23:46:11 +0900

    Update formData.js

--- CSRF_POC/formData.js
+++ CSRF_POC/formData.js
@@ -1,6 +1,6 @@
 const formData = new FormData();
-formData.append("input_pw", "1234567");
-formData.append("input_pw2", "1234567");
+formData.append("input_pw", "12345");
+formData.append("input_pw2", "12345");
 formData.append("input_name", "정순호");
 formData.append("input_mail", "\"><script>alert(1)</script>");
 
